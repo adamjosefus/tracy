575bd6d7f2091c3cf626c63ffd094d27e7f39285
David Grudl <david@grudl.com>
2020-05-19 15:46:34 +0200

    Dumper: uses unicode ellipsis

--- src/Tracy/Dumper/Renderer.php
+++ src/Tracy/Dumper/Renderer.php
@@ -107,6 +107,7 @@ final class Renderer
 			}, $s);
 		}
 		$s = htmlspecialchars_decode(strip_tags($s), ENT_QUOTES);
+		$s = str_replace('…', '...', $s);
 
 		if ($this->sourceLocation && ([$file, $line] = $model->location)) {
 			$s .= "in $file:$line";
@@ -193,7 +194,7 @@ final class Renderer
 			$items = $array;
 			$count = count($items);
 		} elseif ($array->items === null) {
-			return $out . $array->length . ") [ ... ]\n";
+			return $out . $array->length . ") [ … ]\n";
 		} else {
 			$items = $array->items;
 			$count = $array->length ?? count($items);
@@ -233,7 +234,7 @@ final class Renderer
 		}
 
 		if ($count > count($items)) {
-			$out .= $indent . "...\n";
+			$out .= $indent . "…\n";
 		}
 		unset($this->parents[$array->id ?? null]);
 		return $out . '</div>';
@@ -257,7 +258,7 @@ final class Renderer
 			. '</span> <span class="tracy-dump-hash">#' . $object->id . '</span>';
 
 		if ($object->items === null) {
-			return $out . " { ... }\n";
+			return $out . " { … }\n";
 
 		} elseif (!$object->items) {
 			return $out . "\n";
@@ -300,7 +301,7 @@ final class Renderer
 		}
 
 		if ($object->length > count($object->items)) {
-			$out .= $indent . "...\n";
+			$out .= $indent . "…\n";
 		}
 		unset($this->parents[$object->id]);
 		return $out . '</div>';
